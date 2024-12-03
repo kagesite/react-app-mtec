11e6fd277badd90adb1478947b1f60202fe7787a
Kage Saluone <ksaluone17@gmail.com>
2024-12-03 09:12:16 -0700

    Created a Fisher-Yates shuffle function to shuffle answers within each quiz.

--- quiz-app/src/app/components/MultiQuiz.jsx
+++ quiz-app/src/app/components/MultiQuiz.jsx
@@ -150,7 +150,16 @@ function MultiQuiz() {
     const [score, setScore] = useState(0);
     const [showResult, setShowResult] = useState(false);
 
-
+    // Create function to shuffle an array.
+    // I am using the Fisher-Yates shuffle algorithm.
+    function shuffleArray(array) {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i --) {
+            const random = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[random]] = [shuffled[random], shuffled[i]];
+        }
+        return shuffled;
+    }
 
     return (
         <div>
