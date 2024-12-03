b3b64be2128418819998032c3a6981264b48d6bb
Kage Saluone <ksaluone17@gmail.com>
2024-12-03 09:00:08 -0700

    Create new file to test out a multiple quiz app

---
+++ quiz-app/src/app/components/MultiQuiz.jsx
@@ -0,0 +1,76 @@
+"use client"
+import React, { useState } from 'react'
+
+function MultiQuiz() {
+
+    const quizes = [
+        {
+            name: 'Geography',
+            questions: [
+                {
+                    question: 'What is the capital of England?',
+                    answers: [
+                        { text: "London", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+                {
+                    question: 'What is the capital of France?',
+                    answers: [
+                        { text: "Paris", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+                {
+                    question: 'What is the capital of Germany?',
+                    answers: [
+                        { text: "Berlin", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+            ]
+        },
+        {
+            name: 'Math',
+            questions: [
+                {
+                    question: 'What is 9 + 10?',
+                    answers: [
+                        { text: "19", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+                {
+                    question: '45 / 9?',
+                    answers: [
+                        { text: "5", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+                {
+                    question: '96 inches is how many feet?',
+                    answers: [
+                        { text: "8 ft", correct: true },
+                        { text: "Option 2", correct: false },
+                        { text: "Option 3", correct: false },
+                    ]
+                },
+            ]
+        },
+    ]
+    
+    
+    
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default MultiQuiz
