5f0cfa3cf68c684c85782798023ab82e2ddb8a9b
Kage Saluone <ksaluone17@gmail.com>
2024-12-03 09:01:04 -0700

    Removed original TestQuiz element and added the MultiQuiz element instead

--- quiz-app/src/app/page.js
+++ quiz-app/src/app/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import TestQuiz from './components/TestQuiz'
+// import TestQuiz from './components/TestQuiz'
+import MultiQuiz from './components/MultiQuiz'
 
 function Home() {
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
-            <TestQuiz />
+            <MultiQuiz />
         </div>
     )
 }
