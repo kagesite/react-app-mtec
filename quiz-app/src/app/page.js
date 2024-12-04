134c87577e7bd241a0faaeb04561bbc4412775dc
Kage Saluone <ksaluone17@gmail.com>
2024-12-03 22:06:32 -0700

    adjusted Page css

--- quiz-app/src/app/page.js
+++ quiz-app/src/app/page.js
@@ -4,7 +4,7 @@ import MultiQuiz from './components/MultiQuiz'
 
 function Home() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen'>
+        <div className='flex flex-col justify-center items-center h-screen mx-auto p-16'>
             <MultiQuiz />
         </div>
     )
